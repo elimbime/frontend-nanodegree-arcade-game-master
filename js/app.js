4939a08f5cc162777bc521322f084fbe36f8974c
ivan elimbi <elimbime@outlook.com>
2017-07-08 19:48:08 +0200

    Ajout de la vitesse aletoire pour les ennemies Ajout fonction reset

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
+//global variables that represent the square size for our game
+var xstep = 101;
+var ystep = 83;
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
 // Enemies our player must avoid
 var Enemy = function (x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
-    this.xstep = 101;
-    this.ystep = 83;
+    this.xstep = xstep;
+    this.ystep = ystep;
 
     this.xmax = 404;
     this.xmin = 0;
 
     this.ymax = 83*5; 
-    this.ymin = 0; 
+    this.ymin = this.ystep * 5; 
+
+    //this will randomly assign a speed modifier for each enemy created
+    this.speed = getRandomIntInclusive(1, 3); 
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -25,19 +38,21 @@ Enemy.prototype.update = function (dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if (this.x < this.xmax) {
-        this.x = this.x + this.xstep * dt;
+        this.x = this.x + this.xstep * this.speed *dt;
     }
     else {
         this.resetX();
     }
 };
 
+// reset the x postion to the minimum
 Enemy.prototype.resetX = function () {
-        this.x = 0;
+        this.x = this.xmin;
 };
 
+// reset the y postion to the maximum
 Enemy.prototype.resetY = function () {
-        this.y = 0;
+        this.y = this.ymax;
 };
 
 Enemy.prototype.reset = function () {
@@ -72,8 +87,12 @@ Player.prototype.handleInput = function (key) {
             }
             break;
         case 'up':
-            if ( this.y > this.ymin) {
+            if ( this.y > this.ystep) {
                 this.y = this.y - this.ystep;
+            }
+            else
+            {
+                this.resetY();
             }
             break;
         case 'right':
@@ -98,8 +117,8 @@ Player.prototype.update = function () {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-var player = new Player(303, this.ystep * 4);
-var allEnemies = [new Enemy(202, this.ystep * 3),new Enemy(101, this.ystep * 2), new Enemy(303, this.ystep * 1 )];
+var player = new Player(xstep*3, ystep * 4);
+var allEnemies = [new Enemy(xstep*2, ystep * 3),new Enemy(xstep, ystep * 2), new Enemy(xstep*3, ystep * 1 )];
 
 
 // This listens for key presses and sends the keys to your
